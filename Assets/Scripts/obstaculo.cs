5dffa91c20fd6b75c6c04756fa1faf1ab286b26c
Carla Marques <marques.carl97@gmail.com>
2026-03-21 23:18:17 -0300

    animacao da la ursa 1 e 2

--- Assets/Scripts/obstaculo.cs
+++ Assets/Scripts/obstaculo.cs
@@ -4,16 +4,25 @@ public class Obstacle : MonoBehaviour
 {
     public float speed = 2f;
     public float limit = 3f;
-
-    public bool isHorizontal = false; //  controla direção
+    public bool isHorizontal = false;
 
     private Rigidbody2D rb;
     private Vector2 startPosition;
 
+    private Animator animator;
+    private SpriteRenderer sprite;
+
+    private Vector2 lastPosition;
+    private Vector2 direction;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+
         startPosition = transform.position;
+        lastPosition = rb.position;
     }
 
     void FixedUpdate()
@@ -31,6 +40,34 @@ public class Obstacle : MonoBehaviour
             newPosition = new Vector2(startPosition.x, startPosition.y + movement);
         }
 
+        direction = (newPosition - rb.position).normalized;
+
         rb.MovePosition(newPosition);
+    }
+
+    void Update()
+    {
+        bool isMoving = direction.sqrMagnitude > 0.01f;
+        animator.SetBool("isMoving", isMoving);
+
+        if (isMoving)
+        {
+            animator.SetFloat("x", direction.x);
+            animator.SetFloat("y", direction.y);
+
+            if (direction.x > 0)
+            {
+                sprite.flipX = true;
+            }
+            else if (direction.x < 0)
+            {
+                sprite.flipX = false;
+            }
+        }
+        else
+        {
+            animator.SetFloat("x", 0);
+            animator.SetFloat("y", -1);
+        }
     }
 }
